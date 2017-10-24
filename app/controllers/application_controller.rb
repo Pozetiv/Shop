abc9b4fe093acb555c4badadb99a2a24ed131646
Pozetiv <pozetivteam@gmail.com>
2017-10-24 20:24:57 +0300

    fix cart 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def current_order
   	if session[:order_id]
-  		Order.find(session[:order_id])
+  		current_user.order.find(session[:order_id])
   	else
-  		Order.new
+  		current_user.order.new
   end
 end
 end
