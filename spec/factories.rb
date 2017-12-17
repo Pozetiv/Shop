dc891e5ee2b3b2e85767b00e74a696f848699bb8
Roman <pozetivteam@gmail.com>
2017-12-17 22:20:57 +0300

    upd test 2.3

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,6 @@ FactoryGirl.define do
     admin true
   end
 
+
+
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-    describe "Create new products" do
+    describe "CHECK- Create new products" do
       let (:userad) {User.create(name: "admin", email: "adminrs@admin.com", password: "admin1234", admin: true)}
+      let(:cat) {Category.create(name:"First")}
       before do
         visit signin_path
         fill_in "Email", with: userad.email
@@ -11,6 +12,26 @@ RSpec.describe "Products", type: :request do
      end
       subject {page}
       it{should have_link('Creat new item', href: new_product_path)}
-    
+      before {click_link "Creat new item" }
+      it "dont create new item" do
+        visit new_product_path
+        fill_in "Title", with: "Soup"
+        fill_in "Description", with: "Soup with"
+        fill_in "Price", with: "2"
+ ##Need select category for product
+      expect do
+        click_button "Create new product"
+      end.not_to change(Product, :count)
+        expect(page).to have_selector('div.alert')
+        end
     end
+
+      describe "Buy" do
+        let(:cat) {Category.create(name:"First")}
+        let(:product) {Product.create(title: "soup", description: "soup with", price: "10", categoru_id: "1")}
+        it "buy" do
+          visit root_path
+          #click_button "Add to cart"
+        end
+      end
 end
